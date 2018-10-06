1ff95a741955b86a2e1f3151a5f9b0bd5b7caf7c
LauMP92 <redonda.ld@gmail.com>
2018-10-05 22:18:09 -0300

    interfaces

--- practicaObjetos/aniales2lau/src/com/aniales2/Gato.java
+++ practicaObjetos/aniales2lau/src/com/aniales2/Gato.java
@@ -1,11 +1,25 @@
 package com.aniales2;
 
-public class Gato extends Animal {
+public class Gato extends Animal implements Caminante, Alimentarse {
 	
 	public Gato() {
 		
 		palabra="miau";
 		
+	}
+
+	public void transladarse() {
+		
+		
+		System.out.println("el gato trepa y camina");
+		
+		
+		
+	}
+	
+	public void comer() {
+		
+		System.out.println("elgato esta comiendo");
 	}
 
 }
